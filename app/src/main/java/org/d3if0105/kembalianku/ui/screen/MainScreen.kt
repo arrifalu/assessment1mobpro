1e666c5dbb0bbf9fbd2faad10a611477f125ed18
arrifalu <arrifaluthfiatunnisa@gmail.com>
2024-04-03 23:39:46 +0700

    menambahkan radio button dan button count

--- app/src/main/java/org/d3if0105/kembalianku/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if0105/kembalianku/ui/screen/MainScreen.kt
@@ -1,16 +1,27 @@
 package org.d3if0105.kembalianku.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -20,8 +31,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -58,16 +72,26 @@ fun MainScreen() {
 fun ScreenContent(modifier: Modifier) {
     var JumlahUangTunai by remember { mutableStateOf("")}
     var JumlahPembayaran by remember { mutableStateOf("")}
+
+    val radioOptions = listOf(
+        stringResource(id = R.string.sesuai_inputan)
+
+    )
+    var checked by remember { mutableStateOf(radioOptions[0])}
+
     Column (
         modifier = modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(12.dp)
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
             text = stringResource(id = R.string.intro),
             style = MaterialTheme.typography.bodyLarge,
             modifier = Modifier.fillMaxWidth()
+
         )
         OutlinedTextField(value = JumlahUangTunai,
             onValueChange = {JumlahUangTunai = it},
@@ -95,10 +119,68 @@ fun ScreenContent(modifier: Modifier) {
 
 
         )
+        Row (
+
+        ){
+            radioOptions.forEach { text ->
+                checked(
+                    label = text,
+                    isSelected = checked == text,
+                    modifier = Modifier
+                        .selectable(
+                            selected = checked == text,
+                            onClick = { checked = text },
+                            role = Role.RadioButton
+                        )
+                        .weight(1f)
+                        .padding(16.dp)
+                )
+            }
+            Box(
+                contentAlignment = Alignment.Center ){
+            Button(
+                onClick = {},
+                modifier = Modifier.padding(top = 45.dp),
+
+                contentPadding = PaddingValues(horizontal=32.dp,
+                    vertical=16.dp),
+
+
+            ) {
+                Text(text = stringResource(R.string.hitung))
+            }
+                
+            }
+
+
+
+        }
+
+
 
 
     }
 
+
+}
+@Composable
+fun checked(label: String, isSelected: Boolean, modifier: Modifier){
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        RadioButton(selected = isSelected, onClick = null )
+        Text(
+            text = label,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(start = 8.dp)
+        )
+
+    }
+
+}
+private  fun hitungKembalian(cash: Float, totalPembayaran:Float): Float {
+    return cash - totalPembayaran
 
 }
 
