2f2cb5d33d7022feefbae56a88db4bf7c048b7c2
arrifalu <arrifaluthfiatunnisa@gmail.com>
2024-04-05 16:29:30 +0700

    progress akhir

--- app/src/main/java/org/d3if0105/kembalianku/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if0105/kembalianku/ui/screen/MainScreen.kt
@@ -3,18 +3,14 @@ package org.d3if0105.kembalianku.ui.screen
 import android.content.Context
 import android.content.Intent
 import android.content.res.Configuration
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
-
 import androidx.compose.foundation.layout.Arrangement
-
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -24,7 +20,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.Button
-
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -38,9 +33,6 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableIntStateOf
-
-
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -48,10 +40,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.ImeAction
@@ -108,11 +97,11 @@ fun MainScreen(navController: NavHostController) {
 @Composable
 fun ScreenContent(modifier: Modifier) {
     var JumlahUangTunai by rememberSaveable { mutableStateOf("")}
-    var JumlahUangTunaiError by remember { mutableStateOf(false)}
+    var JumlahUangTunaiError by rememberSaveable { mutableStateOf(false)}
 
     var JumlahPembayaran by rememberSaveable { mutableStateOf("")}
-    var JumlahPembayaranError by remember { mutableStateOf(false)}
-    var Kembalian by remember { mutableFloatStateOf(0f) }
+    var JumlahPembayaranError by rememberSaveable { mutableStateOf(false)}
+    var Kembalian by rememberSaveable { mutableFloatStateOf(0f) }
     val context = LocalContext.current
 
 
@@ -173,7 +162,7 @@ fun ScreenContent(modifier: Modifier) {
                 )
         ){
             radioOptions.forEach { text ->
-                checked(
+                Checked(
                     label = text,
                     isSelected = checked == text,
                     modifier = Modifier
@@ -230,7 +219,7 @@ fun ScreenContent(modifier: Modifier) {
 }
 
 @Composable
-fun checked (label: String, isSelected: Boolean, modifier: Modifier){
+fun Checked (label: String, isSelected: Boolean, modifier: Modifier){
     Row(
         modifier = modifier,
         verticalAlignment = Alignment.CenterVertically
